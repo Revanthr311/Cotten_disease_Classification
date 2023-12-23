24bde42132418a0804bbe937f1a115e8bf644cb0
Revanthr311 <revanthr311@gmail.com>
2023-12-23 14:49:50 +0530

    app

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def predictroute():
 if __name__ == "__main__":
     clApp = API()
     # app.run(host='0.0.0.0', port=8080) #local host
-    # app.run(host='0.0.0.0', port=8080) #for AWS
-    app.run(host='0.0.0.0', port=80) #for AZURE
+    app.run(host='0.0.0.0', port=8080) #for AWS
+    #app.run(host='0.0.0.0', port=80) #for AZURE
